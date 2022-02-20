cc55cc00ea2bd2189e69ca13ecc6fdf24b59e0cb
uki <wyspianski.lukasz@gmail.com>
2022-02-20 11:56:35 +0100

    message

---
+++ message/src/main/java/com/example/Message.java
@@ -0,0 +1,30 @@
+package com.example;
+
+public class Message {
+    private String service;
+    private String message;
+
+    public Message() {
+    }
+
+    public Message(String service, String message) {
+        this.service = service;
+        this.message = message;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
